e1d15248840e651030387c451ab29740a2180fd7
Leta Krähenbühl <145375943+leta-krahenbuhl@users.noreply.github.com>
2024-09-17 18:55:14 +0100

    Get user's home route

--- routes/home.js
+++ routes/home.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const Home = require("../models/Home"); // Import Home model
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
 
 router.use(express.json());
+dotenv.config();
+
+const JWT_SECRET = process.env.JWT_SECRET;
 
 // Add Home Route
 router.post("/", async (req, res) => {
@@ -15,7 +20,7 @@ router.post("/", async (req, res) => {
       return res.status(400).json({ message: "Home name already exists" });
     }
 
-    // Create a new home
+    // create a new home
     const newHome = new Home({ homeName, habitants, admins });
     await newHome.save();
 
@@ -23,6 +28,27 @@ router.post("/", async (req, res) => {
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Server error" });
+  }
+});
+
+// Get home of user
+router.get("/user-home", async (req, res) => {
+  try {
+    const token = req.headers.authorization.split(" ")[1]; // Extract token from Authorization header
+    const decoded = jwt.verify(token, JWT_SECRET); // Use your JWT secret to verify the token
+    const username = decoded.username; // Adjust based on your token's structure
+
+    // Find a home where the user is listed as a habitant
+    const home = await Home.findOne({ habitants: username });
+
+    if (home) {
+      return res.json({ homeName: home.homeName });
+    } else {
+      return res.status(404).json({ message: "User is not part of any home." });
+    }
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Server error" });
   }
 });
 
