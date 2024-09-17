5ecbb030e0bdadd18cceb7898ea43d643a258bdf
Leta Krähenbühl <145375943+leta-krahenbuhl@users.noreply.github.com>
2024-09-17 18:17:13 +0100

    Add home route works and updates database

--- models/Home.js
+++ models/Home.js
@@ -3,8 +3,8 @@ const Schema = mongoose.Schema;
 
 const homeSchema = new Schema({
   homeName: { type: String, required: true, unique: true },
-  habitants: { type: String, required: true },
-  admins: { type: String, required: true },
+  habitants: { type: Array, required: true },
+  admins: { type: Array, required: true },
 });
 
 module.exports = mongoose.model("Home", homeSchema);
