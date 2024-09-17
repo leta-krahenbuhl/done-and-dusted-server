1cc5710f2ef76b2a08d3987b08d6cdd19299e4bc
Leta Krähenbühl <145375943+leta-krahenbuhl@users.noreply.github.com>
2024-09-17 18:17:13 +0100

    Add home route works and updates database

--- routes/home.js
+++ routes/home.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../models/Home"); // Import Home model
+const Home = require("../models/Home"); // Import Home model
 
 router.use(express.json());
 
