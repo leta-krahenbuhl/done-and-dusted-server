d4353197ad3171464175aaf8a40467c14cc64af4
Leta Krähenbühl <145375943+leta-krahenbuhl@users.noreply.github.com>
2024-09-18 16:40:00 +0100

    Take away task params

--- models/Task.js
+++ models/Task.js
@@ -4,7 +4,6 @@ const Schema = mongoose.Schema;
 const taskSchema = new Schema({
   taskName: { type: String, required: true },
   minutes: { type: Number, required: true },
-  dateCreated: { type: Date, required: true },
   repeat: { type: String, required: true },
   done: { type: Boolean, required: true },
   doneBy: { type: String },
