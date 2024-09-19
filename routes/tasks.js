243451f4e1e5aade00d516d9891a12518d2068b9
Leta Krähenbühl <145375943+leta-krahenbuhl@users.noreply.github.com>
2024-09-19 16:57:13 +0100

    Edit task route

--- routes/tasks.js
+++ routes/tasks.js
@@ -6,7 +6,7 @@ const dotenv = require("dotenv");
 router.use(express.json());
 dotenv.config();
 
-// Add Task Route
+// Add task route
 router.post("/", async (req, res) => {
   try {
     const { taskName, minutes, repeat, done, homeName, dueDate, week } =
@@ -25,6 +25,43 @@ router.post("/", async (req, res) => {
     await newTask.save();
 
     res.status(201).json({ message: `Task ${taskName} created successfully` });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
+// Edit a task route
+router.patch("/edit", async (req, res) => {
+  try {
+    const { taskName, minutes, repeat, dueDate, taskId } = req.body;
+
+    // Find the task by ID and update it
+    const updatedTask = await Task.findByIdAndUpdate(
+      taskId,
+      {
+        taskName,
+        minutes,
+        repeat,
+        dueDate,
+        taskId,
+      },
+      { new: true, runValidators: true } // Return the updated task and run validations
+    );
+
+    // If task is not found
+    if (!updatedTask) {
+      return res
+        .status(404)
+        .json({ message: `Task with ID ${taskId}not found` });
+    }
+
+    res
+      .status(200)
+      .json({
+        message: `Task ${taskName} updated successfully`,
+        task: updatedTask,
+      });
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Server error" });
