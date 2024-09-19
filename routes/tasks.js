a5a6b32243d94db64b9637c66719e28623c4fde4
Leta Krähenbühl <145375943+leta-krahenbuhl@users.noreply.github.com>
2024-09-19 21:33:17 +0100

    Delete task route

--- routes/tasks.js
+++ routes/tasks.js
@@ -6,7 +6,7 @@ const dotenv = require("dotenv");
 router.use(express.json());
 dotenv.config();
 
-// Add task route
+// Add task
 router.post("/", async (req, res) => {
   try {
     const { taskName, minutes, repeat, done, homeName, dueDate, week } =
@@ -25,6 +25,63 @@ router.post("/", async (req, res) => {
     await newTask.save();
 
     res.status(201).json({ message: `Task ${taskName} created successfully` });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
+// Delete task
+router.delete("/delete", async (req, res) => {
+  try {
+    const { taskId } = req.body;
+    console.log("taskId backend: ", taskId);
+
+    // Find and delete the task by ID
+    const deletedTask = await Task.deleteOne({ _id: taskId });
+
+    // If task is not found or no task was deleted
+    if (deletedTask.deletedCount === 0) {
+      return res
+        .status(404)
+        .json({ message: `Task with ID ${taskId} not found` });
+    }
+
+    res.status(200).json({
+      message: `Task deleted successfully`,
+      taskId: taskId,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
+// Update the done attribute
+router.patch("/update-done", async (req, res) => {
+  try {
+    const { done, taskId } = req.body;
+
+    // Find the task by ID and update it
+    const updatedTask = await Task.findByIdAndUpdate(
+      taskId,
+      {
+        done,
+      },
+      { new: true, runValidators: true } // Return the updated task and run validations
+    );
+
+    // If task is not found
+    if (!updatedTask) {
+      return res
+        .status(404)
+        .json({ message: `Task with ID ${taskId}not found` });
+    }
+
+    res.status(200).json({
+      message: `Task edited (done/undone) successfully`,
+      task: updatedTask,
+    });
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Server error" });
@@ -56,12 +113,10 @@ router.patch("/edit", async (req, res) => {
         .json({ message: `Task with ID ${taskId}not found` });
     }
 
-    res
-      .status(200)
-      .json({
-        message: `Task ${taskName} updated successfully`,
-        task: updatedTask,
-      });
+    res.status(200).json({
+      message: `Task ${taskName} updated successfully`,
+      task: updatedTask,
+    });
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Server error" });
@@ -79,6 +134,26 @@ router.get("/daily-undone", async (req, res) => {
       repeat: "daily",
       week: currentWeekISO,
       done: false,
+    });
+
+    res.json(tasks);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
+// Get tasks of home for this week with repeat 'daily' and done: true
+router.get("/daily-done", async (req, res) => {
+  const { homeName, currentWeekISO } = req.query;
+
+  try {
+    // Find tasks with the specified homeName, repeat set to 'daily', done: true
+    const tasks = await Task.find({
+      homeName: homeName,
+      repeat: "daily",
+      week: currentWeekISO,
+      done: true,
     });
 
     res.json(tasks);
